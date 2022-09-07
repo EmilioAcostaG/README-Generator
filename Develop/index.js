c506db3f155d1915df8e032fb95ea3e431d03009
EmilioAcostaG <103851807+EmilioAcostaG@users.noreply.github.com>
2022-09-07 17:15:33 -0400

    update index.js file with questions.

--- Develop/index.js
+++ Develop/index.js
@@ -4,11 +4,13 @@ const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
-const questions = [
+const prompt = () => {
+  return inquirer.questions([
 {
   type: 'input',
-  name: 'name',
-  message: 'What is your GitHub username?'
+  name: 'username',
+  message: 'What is your GitHub username?',
+  //validate: 
 },
 {
   type: 'input',
@@ -17,46 +19,83 @@ const questions = [
 },
 {
   type: 'input',
-  name: 'Title',
+  name: 'title',
   message: 'What is the title of your project?',
 },
 {
   type: 'input',
-  name: 'Description',
+  name: 'description',
   message: 'Please wrote a description for your project.'
 },
 {
   type: 'input',
-  name: 'Installation',
+  name: 'installation',
   message: 'Please write installation instructions for your project.',
 },
 {
   type: 'input',
-  name: 'Usage',
+  name: 'usage',
   message: 'Please write usage information for your project.',
 },
 {
   type: 'input',
-  name: 'Contributing',
+  name: 'contribution',
   message: 'Please write contribution guidelines for your project.',
 },
 {
   type: 'input',
-  name: 'Tests',
+  name: 'test',
   message: 'Please write test instructions for your project.',
 },
 {
   type: 'list',
-  name: 'Licence',
+  name: 'licence',
   message: 'Please choose a license for your project.',
   choices: ['MIT', 'Apache 2.0', 'GPLv3', 'None']
 },
-];
+])};
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+const generateReadMe = ({
+  username,
+  email,
+  title,
+  description,
+  installation,
+  usage,
+  contribution,
+  test,
+  license
+}) =>
+  `#${title}
 
-// Function call to initialize app
-init();
+  ## Description
+  ${description}
+  
+  ## Installation
+  ${installation}
+  
+  ## Usage
+  ${usage}
+  
+  ## Contributions
+  ${contribution}
+  
+  ## License
+  Ths project is licensed under the ${license} license.
+  
+  ### Questions
+  My GitHub username is ${username} and you can find my page [here](github.com/${username}).
+  You can also reach me via email to: ${email}.`
+
+
+
+// function writeToFile(fileName, data) {}
+// fs.writeFile('log.txt', process.argv[2], (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
